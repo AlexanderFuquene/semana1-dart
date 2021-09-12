cfad523df0274958b017b44a74678df7e27d437c
AlexanderFuquene <hover.unillanos@hotmail.com>
2021-09-12 18:09:34 -0500

    actualizado

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -4,7 +4,13 @@ void main() {
   var edad = 27;
   int edad_dos = 28;
   var test;
+  test = 50;
+//null safety
+  int? variableNull = null;
 
+//final - constante
+  final String variableFinalTest = "hola";
+  const String variableConstanteTest = "hola";
   print(nombre_dos.toUpperCase());
   print(nombre);
 }
