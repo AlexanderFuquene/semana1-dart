1fa09d4aa6eb0cd446359913674588df6a751217
Hover Fuquene <hover.unillanos@hotmail.com>
2021-09-12 17:46:58 -0500

    actualizar

---
+++ 1.variables/variables.dart
@@ -0,0 +1,10 @@
+void main() {
+  var nombre = "Hover";
+  String nombre_dos = "Alex";
+  var edad = 27;
+  int edad_dos = 28;
+  var test;
+
+  print(nombre_dos.toUpperCase());
+  print(nombre);
+}
